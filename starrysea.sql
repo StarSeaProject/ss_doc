f2cd4c1cfb2b8076a56cb3969845833a230e6a33
xiongjunlang <xiongjunlang@agree.com.cn>
2017-11-13 11:16:12 +0800

    完成设计文档;增加数据库设计以及数据库建库脚本

---
+++ starrysea.sql
@@ -0,0 +1,128 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : 本地
+Source Server Version : 50719
+Source Host           : localhost:3306
+Source Database       : starrysea
+
+Target Server Type    : MYSQL
+Target Server Version : 50719
+File Encoding         : 65001
+
+Date: 2017-11-13 11:15:21
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for activity
+-- ----------------------------
+DROP TABLE IF EXISTS `activity`;
+CREATE TABLE `activity` (
+  `activity_id` int(11) NOT NULL AUTO_INCREMENT,
+  `activity_name` varchar(30) NOT NULL,
+  `activity_content` text NOT NULL,
+  `activity_status` tinyint(4) NOT NULL COMMENT '1-未开始\r\n2-进行中\r\n3-已结束',
+  `activity_qrcode` varchar(50) NOT NULL,
+  PRIMARY KEY (`activity_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for admin
+-- ----------------------------
+DROP TABLE IF EXISTS `admin`;
+CREATE TABLE `admin` (
+  `admin_id` int(11) NOT NULL AUTO_INCREMENT,
+  `admin_useraccount` varchar(10) NOT NULL,
+  `admin_password` varchar(15) NOT NULL,
+  PRIMARY KEY (`admin_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for area
+-- ----------------------------
+DROP TABLE IF EXISTS `area`;
+CREATE TABLE `area` (
+  `area_id` int(11) NOT NULL AUTO_INCREMENT,
+  `city_id` int(11) NOT NULL,
+  `area_name` varchar(10) NOT NULL,
+  PRIMARY KEY (`area_id`),
+  KEY `city_id` (`city_id`),
+  CONSTRAINT `area_ibfk_1` FOREIGN KEY (`city_id`) REFERENCES `city` (`city_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for city
+-- ----------------------------
+DROP TABLE IF EXISTS `city`;
+CREATE TABLE `city` (
+  `city_id` int(11) NOT NULL AUTO_INCREMENT,
+  `province_id` int(11) NOT NULL,
+  `city_name` varchar(10) NOT NULL,
+  PRIMARY KEY (`city_id`),
+  KEY `province_id` (`province_id`),
+  CONSTRAINT `city_ibfk_1` FOREIGN KEY (`province_id`) REFERENCES `province` (`province_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for online
+-- ----------------------------
+DROP TABLE IF EXISTS `online`;
+CREATE TABLE `online` (
+  `online_id` varchar(10) NOT NULL,
+  `online_email` varchar(50) NOT NULL,
+  PRIMARY KEY (`online_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for order
+-- ----------------------------
+DROP TABLE IF EXISTS `order`;
+CREATE TABLE `order` (
+  `order_id` varchar(10) NOT NULL,
+  `order_num` varchar(30) NOT NULL,
+  `order_name` varchar(10) NOT NULL,
+  `order_area` int(11) NOT NULL,
+  `order_address` varchar(50) NOT NULL,
+  `order_stauts` tinyint(4) NOT NULL COMMENT '1-未发货\r\n2-已发货',
+  PRIMARY KEY (`order_id`),
+  KEY `order_area` (`order_area`),
+  CONSTRAINT `order_ibfk_1` FOREIGN KEY (`order_area`) REFERENCES `area` (`area_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for province
+-- ----------------------------
+DROP TABLE IF EXISTS `province`;
+CREATE TABLE `province` (
+  `province_id` int(11) NOT NULL AUTO_INCREMENT,
+  `province_name` varchar(10) NOT NULL,
+  PRIMARY KEY (`province_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for work
+-- ----------------------------
+DROP TABLE IF EXISTS `work`;
+CREATE TABLE `work` (
+  `work_id` int(11) NOT NULL AUTO_INCREMENT,
+  `work_name` varchar(30) NOT NULL,
+  `work_uploadtime` date NOT NULL,
+  `work_pdfpath` varchar(50) NOT NULL,
+  `work_stock` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`work_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for work_image
+-- ----------------------------
+DROP TABLE IF EXISTS `work_image`;
+CREATE TABLE `work_image` (
+  `work_image_id` int(11) NOT NULL AUTO_INCREMENT,
+  `work_id` int(11) NOT NULL,
+  `work_image_path` varchar(50) NOT NULL,
+  PRIMARY KEY (`work_image_id`),
+  KEY `work_id` (`work_id`),
+  CONSTRAINT `work_image_ibfk_1` FOREIGN KEY (`work_id`) REFERENCES `work` (`work_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
