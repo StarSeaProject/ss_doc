bd6416188ee3a1bef691e15c6e1322080ff588f9
xiongjunlang <xiongjunlang@agree.com.cn>
2017-11-17 14:39:18 +0800

    更新数据库脚本

--- starrysea.sql
+++ starrysea.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50719
 File Encoding         : 65001
 
-Date: 2017-11-13 11:15:21
+Date: 2017-11-17 14:38:36
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -50,7 +50,7 @@ CREATE TABLE `area` (
   PRIMARY KEY (`area_id`),
   KEY `city_id` (`city_id`),
   CONSTRAINT `area_ibfk_1` FOREIGN KEY (`city_id`) REFERENCES `city` (`city_id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for city
@@ -63,7 +63,7 @@ CREATE TABLE `city` (
   PRIMARY KEY (`city_id`),
   KEY `province_id` (`province_id`),
   CONSTRAINT `city_ibfk_1` FOREIGN KEY (`province_id`) REFERENCES `province` (`province_id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for online
@@ -76,19 +76,21 @@ CREATE TABLE `online` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Table structure for order
+-- Table structure for orders
 -- ----------------------------
-DROP TABLE IF EXISTS `order`;
-CREATE TABLE `order` (
+DROP TABLE IF EXISTS `orders`;
+CREATE TABLE `orders` (
   `order_id` varchar(10) NOT NULL,
   `order_num` varchar(30) NOT NULL,
   `order_name` varchar(10) NOT NULL,
   `order_area` int(11) NOT NULL,
   `order_address` varchar(50) NOT NULL,
-  `order_stauts` tinyint(4) NOT NULL COMMENT '1-未发货\r\n2-已发货',
+  `order_status` tinyint(4) NOT NULL COMMENT '1-未发货\r\n2-已发货',
+  `order_expressnum` varchar(30) DEFAULT NULL,
+  `order_time` bigint(10) unsigned NOT NULL,
   PRIMARY KEY (`order_id`),
   KEY `order_area` (`order_area`),
-  CONSTRAINT `order_ibfk_1` FOREIGN KEY (`order_area`) REFERENCES `area` (`area_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`order_area`) REFERENCES `area` (`area_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
@@ -99,7 +101,7 @@ CREATE TABLE `province` (
   `province_id` int(11) NOT NULL AUTO_INCREMENT,
   `province_name` varchar(10) NOT NULL,
   PRIMARY KEY (`province_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for work
@@ -112,7 +114,7 @@ CREATE TABLE `work` (
   `work_pdfpath` varchar(50) NOT NULL,
   `work_stock` int(10) unsigned NOT NULL,
   PRIMARY KEY (`work_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for work_image
