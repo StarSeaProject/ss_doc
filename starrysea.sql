9abe916125521d7133b2dc29c6d9898cbfcc33f0
xiongjunlang <xiongjunlang@agree.com.cn>
2017-11-17 14:49:00 +0800

    为兼容MD5增加管理员密码字段的长度

--- starrysea.sql
+++ starrysea.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50719
 File Encoding         : 65001
 
-Date: 2017-11-17 14:38:36
+Date: 2017-11-17 14:48:36
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -35,7 +35,7 @@ DROP TABLE IF EXISTS `admin`;
 CREATE TABLE `admin` (
   `admin_id` int(11) NOT NULL AUTO_INCREMENT,
   `admin_useraccount` varchar(10) NOT NULL,
-  `admin_password` varchar(15) NOT NULL,
+  `admin_password` varchar(35) NOT NULL,
   PRIMARY KEY (`admin_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
